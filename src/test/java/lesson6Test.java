273efef2c46f6dd93ca363f348b0cac8d9ad659d
FlippeRRG <QWeAsdZxc123008>
2018-08-28 01:18:01 +0300

    task1,2,3

--- src/test/java/lesson6Test.java
+++ src/test/java/lesson6Test.java
@@ -1,11 +1,16 @@
 import junit.framework.Assert;
-import org.junit.*;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.Arrays;
 
 class lesson6Test {
+    lesson6 ls6 = new lesson6();
+    private static Connection connection;
+    private static Statement stmt;
+    private static PreparedStatement pstmt;
 
     @Test
     void task1() {
@@ -20,4 +25,32 @@ class lesson6Test {
         int[] test = {1,4};
         Assert.assertEquals( true, lesson6.task2( test ) );
     }
+
+    @Test
+    void insert() {
+        lesson6.createDB();
+        lesson6.insert( "Ivanov", 5 );
+        int lastID = lesson6.readID();
+        Assert.assertEquals( "Ivanov 5", lesson6.read( lastID ) );
+        lesson6.delete( lastID );
+    }
+    @Test
+    void updateSurnameTest(){
+        lesson6.createDB();
+        lesson6.insert( "Ivanov", 5 );
+        int lastID = lesson6.readID();
+        lesson6.updateSurname( "Petrov", lastID );
+        Assert.assertEquals( "Petrov 5", lesson6.read( lastID ) );
+        lesson6.delete( lastID );
+    }
+    @Test
+    void updateScoreTest(){
+        lesson6.createDB();
+        lesson6.insert( "Ivanov", 5 );
+        int lastID = lesson6.readID();
+        lesson6.updateScore( 0, lastID );
+        Assert.assertEquals( "Ivanov 0", lesson6.read( lastID ) );
+        lesson6.delete( lastID );
+    }
+
 }
